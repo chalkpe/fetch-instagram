ca0f9dc5b272a81ce1d3fa28b9e5472f7b555297
ChalkPE <chalk@chalk.pe>
2017-05-19 00:28:29 +0900

    Moved files

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const exec = require('child_process').exec
+
+const root = path.resolve(__dirname, '..')
+const source = path.resolve(root, 'dist', 'fetch.js')
+const phantom = path.resolve(root, 'node_modules', '.bin', 'phantomjs')
+
+const express = require('express')
+const app = express().use(require('./logger')())
+
+app.get('/:tag', (req, res) => {
+  const tag = encodeURIComponent(req.params.tag)
+  exec(`${phantom} ${source} ${tag}`, (err, out) =>
+    res.json(err ? { ok: false, message: err.message } : JSON.parse(out)))
+})
+
+const port = process.env.PORT || '2409'
+app.listen(port, () => console.log(`Listening on port ${port}`))
